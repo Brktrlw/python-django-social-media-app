17c2e4653a6cfdec7f0bbd9b35d4425e4547783a
BrktrL <74716514+Brktrlw@users.noreply.github.com>
2021-10-25 11:36:30 +0300

    Update

--- User/forms.py
+++ User/forms.py
@@ -10,15 +10,12 @@ class LoginForm(forms.Form):
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control form-control-lg'
 class RegisterForm(forms.Form):
-    username=forms.CharField(max_length=50,label="Kullanıcı Adı",required=True,widget=forms.TextInput(attrs={"placeholder":"Kullanıcı adınızı giriniz"}))
-    email   =forms.CharField(max_length=100,label="E posta Adresi",required=True,widget=forms.TextInput(attrs={"placeholder":"E posta adresinizi giriniz"}))
-    password=forms.CharField(max_length=100,label="Parola",required=True,widget=forms.PasswordInput(attrs={"placeholder":"Parolanınızı giriniz"}))
-    confirm = forms.CharField(max_length=50, label="Parolayı Doğrula", required=True,widget=forms.PasswordInput(attrs={"placeholder":"Parolanınızı tekrar giriniz"}))
+    username=forms.CharField(max_length=50,label="Kullanıcı Adı",required=True,widget=forms.TextInput(attrs={"placeholder":"Kullanıcı adınızı giriniz","class":"form-control form-control-lg"}))
+    email   =forms.CharField(max_length=100,label="E posta Adresi",required=True,widget=forms.TextInput(attrs={"placeholder":"E posta adresinizi giriniz","class":"form-control form-control-lg"}))
+    password=forms.CharField(max_length=100,label="Parola",required=True,widget=forms.PasswordInput(attrs={"placeholder":"Parolanınızı giriniz","class":"form-control form-control-lg"}))
+    confirm = forms.CharField(max_length=50, label="Parolayı Doğrula", required=True,widget=forms.PasswordInput(attrs={"placeholder":"Parolanınızı tekrar giriniz","class":"form-control form-control-lg"}))
+    
 
-    def __init__(self, *args, **kwargs):
-        super(RegisterForm, self).__init__(*args, **kwargs)
-        for visible in self.visible_fields():
-            visible.field.widget.attrs['class'] = 'form-control form-control-lg'
 
     def clean(self):
         username = self.cleaned_data.get("username")
